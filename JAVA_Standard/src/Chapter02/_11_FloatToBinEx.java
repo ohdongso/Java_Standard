cead0d617cac0c28422f08a6d64f0284b4760975
dongso <ehdth5806@naver.com>
2023-08-07 15:58:04 +0900

    자바의 정석 5번째 수업 Commit.

---
+++ JAVA_Standard/src/Chapter02/_11_FloatToBinEx.java
@@ -0,0 +1,12 @@
+package Chapter02;
+
+public class _11_FloatToBinEx {
+
+	public static void main(String[] args) {
+		float f = 9.1234567f;
+		int i = Float.floatToIntBits(f);
+		
+		System.out.printf("%f%n", f);
+		System.out.printf("%X%n", i); // 16진수로 출력
+	} // main의 끝
+}
---
+++ JAVA_Standard/src/Chapter02/_12_CastingEx1.java
@@ -0,0 +1,12 @@
+package Chapter02;
+
+public class _12_CastingEx1 {
+
+	public static void main(String[] args) {
+		double d = 85.4;
+		int score = (int)d;
+		
+		System.out.println("score=" + score);
+		System.out.println("d=" + d);
+	}
+}
