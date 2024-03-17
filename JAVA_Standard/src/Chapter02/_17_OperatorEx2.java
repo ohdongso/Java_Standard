6e06baf883a8fc90e10eadf7ef7238b97484f31a
dongso <ehdth5806@naver.com>
2024-03-17 23:56:33 +0900

    자바의 정석 8번째 수업 Commit.

---
+++ JAVA_Standard/src/Chapter02/_17_OperatorEx2.java
@@ -0,0 +1,16 @@
+package Chapter02;
+
+public class _17_OperatorEx2 {
+
+	public static void main(String[] args) {
+		int i = 5, j = 0;
+		j = i++;
+		System.out.println("j=i++; 실행 후, i=" + i + ", j=" + j);
+		
+		i = 5;
+		j = 0;
+		
+		j = ++i;
+		System.out.println("j=++i; 실행 후, i=" + i + ", j=" + j);
+	}
+}
---
+++ JAVA_Standard/src/Chapter02/_18_OperatorEx3.java
@@ -0,0 +1,11 @@
+package Chapter02;
+
+public class _18_OperatorEx3 {
+
+	public static void main(String[] args) {
+		int i = 5, j = 5;
+		System.out.println(i++);
+		System.out.println(++j);
+		System.out.println("i = " + i + ", j = " + j);
+	}
+}
