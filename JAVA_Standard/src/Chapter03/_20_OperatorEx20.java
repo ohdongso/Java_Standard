b5ac11aa2d64f6d025f07def4c0d4d0c52a62ee2
dongso <ehdth5806@naver.com>
2024-03-21 22:59:28 +0900

    자바의 정석 11번째 수업 Commit.

---
+++ JAVA_Standard/src/Chapter03/_20_OperatorEx20.java
@@ -0,0 +1,11 @@
+package Chapter03;
+
+public class _20_OperatorEx20 {
+
+	public static void main(String[] args) {
+		// 부호 무시 후나머지연산 뒤 좌측 부호를 붙인다.
+		System.out.println(-10 % 8);
+		System.out.println(10 % -8);
+		System.out.println(-10 % -8);
+	}
+}
