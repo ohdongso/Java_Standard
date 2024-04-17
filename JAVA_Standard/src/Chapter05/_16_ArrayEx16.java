54df512ddea01de74c4ef8c6521aa708f0a0e2e0
dongso <ehdth5806@naver.com>
2024-04-17 19:05:22 +0900

    자바의정석 32번째 수업 Commit.

---
+++ JAVA_Standard/src/Chapter05/_16_ArrayEx16.java
@@ -0,0 +1,11 @@
+package Chapter05;
+
+public class _16_ArrayEx16 {
+	
+	public static void main(String[] args) {
+		System.out.println("매개변수의 개수:" + args.length);
+		for(int i = 0; i < args.length; i++) {
+			System.out.println("args[" + i + "] = \"" + args[i] + "\"");
+		}
+	}
+}
---
+++ JAVA_Standard/src/Chapter05/_17_ArrayEx17.java
@@ -0,0 +1,14 @@
+package Chapter05;
+
+public class _17_ArrayEx17 {
+	
+	public static void main(String[] args) {
+		if(args.length != 3) {
+			System.out.println("usage: java Array17 MUMI OP NUM2");
+		}
+		
+		
+		
+		
+	}
+}
