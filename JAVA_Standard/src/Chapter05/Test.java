8a5e457d311dfca9322db3cfd0038d348afee41e
dongso <ehdth5806@naver.com>
2024-04-18 22:06:36 +0900

    자바의정석 33번째 수업 Commit.

--- JAVA_Standard/src/Chapter05/Test.java
+++ JAVA_Standard/src/Chapter05/Test.java
@@ -3,12 +3,6 @@ package Chapter05;
 public class Test {
 
 	public static void main(String[] args) {
-//		System.out.println((int)'C');
-//		System.out.println((int)'A');
-//		System.out.println((int)'F');
-//		System.out.println((int)'E');
-		
-		System.out.println((int)'0');
-		System.out.println((int)'9');
+		System.out.println((char)(1 + '0'));
 	}
 }
