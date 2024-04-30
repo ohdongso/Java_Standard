354d17d1abaed672bb73850d9c3c895bf87888a2
dongso <ehdth5806@naver.com>
2024-04-30 22:38:36 +0900

    자바의정석 44번째 수업 Commit.

---
+++ JAVA_Standard/src/Chapter06/_19_MyMathTest2.java
@@ -0,0 +1,38 @@
+package Chapter06;
+
+class MyMath2 {
+	long a, b;
+	
+	// 인스턴스변수 a, b만을 이용해서 작업하므로 매개변수가 필요없다.
+	long add() { return a + b; } // a, b는 인스턴스변수
+	long subtract() { return a - b; }
+	long multiply() { return a * b; }
+	double divide() { return a / b; }
+	
+	// 인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
+	static long add(long a, long b) { return a + b; } // a, b는 지역변수
+	static long subtract(long a, long b) { return a - b; }
+	static long multiply(long a, long b) { return a * b; }
+	static double divide(double a, double b) { return a / b; }
+}
+
+public class _19_MyMathTest2 {
+	
+	public static void main(String[] args) {
+		// 클래스메서드 호출. 인스턴스 생성없이 호출가능
+		System.out.println(MyMath2.add(200L, 100L)); // static long add(long a, long b)
+		System.out.println(MyMath2.subtract(200L, 100L)); // static long subtract(long a, long b)
+		System.out.println(MyMath2.multiply(200L, 100L)); // static long multiply(long a, long b)
+		System.out.println(MyMath2.divide(200.0, 100.0)); // static double divide(double a, double b)
+		
+		MyMath2 mm = new MyMath2(); // 인스턴스 생성
+		mm.a = 200L;
+		mm.b = 100L;
+		
+		// 인스턴스메서드는 객체생성 후에만 호출이 가능함.
+		System.out.println(mm.add()); // long add() { return a + b; }
+		System.out.println(mm.subtract()); // long subtract() { return a - b; }
+		System.out.println(mm.multiply()); // long multiply() { return a * b; }
+		System.out.println(mm.divide());// double divide() { return a / b; }
+	}
+}
