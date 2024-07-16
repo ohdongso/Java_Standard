31eb4124e2495d60abffd3a53448491541fdaa1d
dongso <ehdth5806@naver.com>
2024-07-16 22:21:25 +0900

    자바의정석 97번째 Commit.

--- JAVA_Standard/src/Chapter09/_02_EqualsEx2.java
+++ JAVA_Standard/src/Chapter09/_02_EqualsEx2.java
@@ -3,10 +3,12 @@ package Chapter09;
 class Person {
 	long id;
 		
-		public boolean equals(Object obj) {
-			if(obj != null && obj instanceof Person)
-				return id == ((Person)obj).id;
+		public boolean equals(Object obj) { // p2가 들어왔다.(업캐스팅)
+			if(obj != null && obj instanceof Person) // obj가 Person으로 형변환이 가능하면
+				// obj가 Object타입이므로 id값을 참조하기 위해서는 Person타입으로 형변환이 필요하다.
+				return id == ((Person)obj).id; // 다운캐스팅 한 뒤 p2의 id값을 가져와서, p1의 id값과 비교한다. 
 			else
+				// 타입이 Person이 아니면 값을 비교할 필요도 없다.
 				return false;
 		}
 		
@@ -21,12 +23,12 @@ public class _02_EqualsEx2 {
 		Person p1 = new Person(8011081111222L);
 		Person p2 = new Person(8011081111222L);
 	
-		if(p1 == p2)
+		if(p1 == p2) // 주소 값 비교
 			System.out.println("p1과 p2는 같은 사람입니다.");
 		else
 			System.out.println("p1과 p2는 다른 사람입니다.");
 		
-		if(p1.equals(p2))
+		if(p1.equals(p2)) // 객체내부 id 문자열 비교
 			System.out.println("p1과 p2는 같은 사람입니다.");
 		else
 			System.out.println("p1과 p2는 다른 사람입니다.");
