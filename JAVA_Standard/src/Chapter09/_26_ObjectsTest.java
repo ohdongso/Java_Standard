f4def72bb16862dce5f617f54eb751267b974333
dongso <ehdth5806@naver.com>
2024-08-17 23:23:40 +0900

    자바의정석 120번째 Commit.

--- JAVA_Standard/src/Chapter09/_26_ObjectsTest.java
+++ JAVA_Standard/src/Chapter09/_26_ObjectsTest.java
@@ -1,39 +1,26 @@
 package Chapter09;
 
-import java.util.*;
-import static java.util.Objects.*; // Objects클래스의 메서드를 static import
+import java.util.Random;
 
 public class _26_ObjectsTest {
-
+	/*
+		Random인스턴스 rand와 rand2가 같은 종자값(seed)을 사용하기 때문에 같은 값들을 같은 순서로 얻는 것을 확인할 수 있다.
+		여러분의 컴퓨터에서도 위와 같은 실행결과를 얻을 수 있을 것이다. 같은 종자값을 갖는 Random인스턴스는 시스템이나
+		실행시간 등에 관계없이 항상 같은 값은 순서로 반환할 것을 보장한다.
+	*/
 	public static void main(String[] args) {
-		String[][] str2D = new String[][] {{"aaa","bbb"},{"AAA", "BBB"}}; // 2행 2열
-		String[][] str2D_2 = new String[][] {{"aaa","bbb"},{"AAA","BBB"}};
+		Random rand = new Random(1); // 매개변수seed를 종자값으로 하는 Random인스턴스를 생성한다.
+		Random rand2 = new Random(1);
 		
-		System.out.print("str2D	={");
-		for(String[] tmp : str2D) { // 행이 추출된다.
-			System.out.print(Arrays.toString(tmp)); // 추출된 행 배열을 출력한다.
-		} // for문 끝.
-		System.out.println("}");
-		
-		System.out.print("str2D_2={");
-		for(String[] tmp : str2D_2) {
-			System.out.print(Arrays.toString(tmp));
+		System.out.println("= rand = ");
+		for(int i = 0; i < 5; i++) {
+			System.out.println(i + ":" + rand.nextInt());
 		}
-		System.out.println("}");
 		
-		System.out.println("equals(str2D, str2D_2)=" + Objects.equals(str2D, str2D_2));
-		System.out.println("deepEquals(str2D, str2D_2)=" + Objects.deepEquals(str2D, str2D_2));
-		
-		System.out.println("isNull(null)  =" + isNull(null));
-		System.out.println("nonNull(null) =" + nonNull(null));
-		System.out.println("hashCode(null)=" + Objects.hashCode(null));
-		System.out.println("toString(null)=" + Objects.toString(null));
-		System.out.println("toString(null, \"\")=");
-		
-		Comparator c = String.CASE_INSENSITIVE_ORDER; // 대소문자 구분 안하는 비교
-		
-		System.out.println("compare(\"aa\",\"bb\")=" + compare("aa", "bb", c));
-		System.out.println("compare(\"bb\",\"aa\")=" + compare("bb", "aa", c));
-		System.out.println("compare(\"bb\",\"AB\")=" + compare("ab", "AB", c)); // 대소문자를 구분하지지 않고 문자열 비교
+		System.out.println();
+		System.out.println("= rand2 =");
+		for(int i = 0; i < 5; i++) {
+			System.out.println(i + ":" + rand2.nextInt());
+		}
 	} // main 끝.
 }
