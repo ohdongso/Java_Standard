f050f6e781e8847ea2552424fa00c245c8bf87ce
dongso <ehdth5806@naver.com>
2024-08-31 22:42:43 +0900

    자바의정석 130번째 Commit.

--- JAVA_Standard/src/Chapter09/_37_ScannerEx3.java
+++ JAVA_Standard/src/Chapter09/_37_ScannerEx3.java
@@ -1,6 +1,6 @@
 package Chapter09;
 
-import java.io.File; 
+import java.io.File;  
 import java.util.Scanner;
 
 public class _37_ScannerEx3 {
@@ -13,12 +13,12 @@ public class _37_ScannerEx3 {
 		int totalSum = 0;
 		
 		while (sc.hasNextInt()) { // Scanner 객체에 입력값이 int값일 때만 true를 반환
-			String line = sc.nextLine();
-			Scanner sc2 = new Scanner(line).useDelimiter(",");
+			String line = sc.nextLine(); // 한 줄을 입력받는다.
+			Scanner sc2 = new Scanner(line).useDelimiter(","); // 구분자를 기준으로 숫자를 구분한다.
 			int sum = 0;
 			
-			while(sc2.hasNextInt()) {
-				sum += sc.nextInt(); // int를 한 줄씩 입력받음
+			while(sc2.hasNextInt()) { // 각각의 숫자를 뽑아내서 누적한다.
+				sum += sc2.nextInt();
 			}
 			System.out.println(line + ", sum = " + sum);
 			totalSum += sum;
