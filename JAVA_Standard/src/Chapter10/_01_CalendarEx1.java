3182f1ebeb3f28c54d47594513174fbc8ddd106e
dongso <ehdth5806@naver.com>
2024-09-13 23:41:46 +0900

    자바의정석 140번째 Commit.

--- JAVA_Standard/src/Chapter10/_01_CalendarEx1.java
+++ JAVA_Standard/src/Chapter10/_01_CalendarEx1.java
@@ -19,5 +19,14 @@ public class _01_CalendarEx1 {
 		System.out.println("이 해의 몇 일: " + today.get(Calendar.DAY_OF_YEAR));
 		System.out.println("요일(1~7, 1:일요일): " + today.get(Calendar.DAY_OF_WEEK)); // 1:일요일, 2:월요일, ... 7:토요일
 		
+		System.out.println("이 달의 몇 째 요일: " + today.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+		System.out.println("오전_오후(0:오전, 1:오후): " + today.get(Calendar.AM_PM));
+		System.out.println("시간(0~11): " + today.get(Calendar.HOUR));
+		System.out.println("시간(0~23): " + today.get(Calendar.HOUR_OF_DAY));
+		System.out.println("분(0~59): " + today.get(Calendar.MINUTE));
+		System.out.println("1000분의 1초(0~999): " + today.get(Calendar.MILLISECOND));
+		
+		// 천분의 1초를 시간으로 표시하기 위해 3600000으로 나누었다.(1시간 = 60 * 60초)
+		System.out.println("이 달의 마지막 날: " + today.getActualMaximum(Calendar.DATE)); // 이달의 마지막 일을 찾는다.
 	}
-}
+} 
