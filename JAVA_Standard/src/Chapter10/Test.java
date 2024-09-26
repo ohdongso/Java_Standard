f1485cfed934e8c72ee0c7d3170d5b84a27f3ce8
dongso <ehdth5806@naver.com>
2024-09-26 23:45:14 +0900

    자바의정석 149번째 Commit.

--- JAVA_Standard/src/Chapter10/Test.java
+++ JAVA_Standard/src/Chapter10/Test.java
@@ -1,15 +1,8 @@
 package Chapter10;
 
-import java.util.Calendar;
-
 public class Test {
 
 	public static void main(String[] args) {
-		Calendar sDay = Calendar.getInstance();
-		Calendar eDay = Calendar.getInstance();
-		sDay.set(2024, 8, 1);
-		eDay.set(2024, 8, 30);
 		
-		System.out.println(eDay.get(Calendar.DAY_OF_WEEK));
 	}
 }
---
+++ JAVA_Standard/src/Chapter10/_09_CalendarEx9.java
@@ -0,0 +1,31 @@
+package Chapter10;
+
+public class _09_CalendarEx9 {
+	/*
+		
+	 */
+	public static void main(String[] args) {
+		
+	} // main
+
+	public static int[] endOfMonth = {};
+	public static boolean isLeapYear(int year) {
+		return true;
+	}
+	
+	public static int dayDiff(int yl, int ml, int dl, int y2, int m2, int d2) {
+		return 0;
+	}
+	
+	public static int getDayOfWeek(int year, int month, int day) {
+		return 0;
+	}
+	
+	public static String convertDayToDate(int day) {
+		return "";
+	}
+	
+	public static int convertDateToDay(int year, int month, int day) {
+		return 0;
+	}
+}
