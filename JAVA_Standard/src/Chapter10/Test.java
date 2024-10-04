052d39f3b35e15c4a1d22e014501a7803fd6acd4
dongso <ehdth5806@naver.com>
2024-10-04 23:31:34 +0900

    자바의정석 152번째 Commit.

--- JAVA_Standard/src/Chapter10/Test.java
+++ JAVA_Standard/src/Chapter10/Test.java
@@ -2,7 +2,7 @@ package Chapter10;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception{
+		double number = 1234567.89;
 	}
 }
