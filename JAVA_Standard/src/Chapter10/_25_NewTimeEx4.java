30b8eaa5c2d55313c161ed1e9b038b6843a2dd5c
dongso <ehdth5806@naver.com>
2024-11-06 21:39:05 +0900

    자바의정석 169번째 Commit

--- JAVA_Standard/src/Chapter10/_25_NewTimeEx4.java
+++ JAVA_Standard/src/Chapter10/_25_NewTimeEx4.java
@@ -1,40 +1,28 @@
 package Chapter10;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class _25_NewTimeEx4 {
+	
 	public static void main(String[] args) {
-		LocalDate date1 = LocalDate.of(2014, 1, 1);
-		LocalDate date2 = LocalDate.of(2015, 12, 31);
+		ZonedDateTime zdateTime = ZonedDateTime.now();
 		
-		Period pe = Period.between(date1, date2);
+		String[] patternArr = {
+			"yyyy-MM-dd HH:mm:ss",
+			"''yy년 MMM dd일 E요일",
+			"yyyy-MM-dd HH:mm:ss.SSS Z VV",
+			"yyyy-MM-dd hh:mm:ss a",
+			"오늘은 올 해의 D번째 날입니다.",
+			"오늘은 이 달의 d번째 날입니다.",
+			"오늘은 올 해의 w번째 주입니다.",
+			"오늘은 이 달의 w번째 주입니다.",
+			"오늘은 이 달의 w번째 E요일입니다."
+		};
 		
-		System.out.println("date1=" + date1);
-		System.out.println("date2=" + date2);
-		System.out.println("pe=" + pe);
-		
-		System.out.println("YEAR=" + pe.get(ChronoUnit.YEARS));
-		System.out.println("MONTH=" + pe.get(ChronoUnit.MONTHS));
-		System.out.println("DAY=" + pe.get(ChronoUnit.DAYS));
-		
-		LocalTime time1 = LocalTime.of(0, 0, 0);
-		LocalTime time2 = LocalTime.of(12, 34, 56); // 12시간 23분 56초
-		
-		Duration du = Duration.between(time1, time2);
-		
-		System.out.println("time1=" + time1);
-		System.out.println("time2=" + time2);
-		System.out.println("du=" + du);
-		
-		LocalTime tmpTime = LocalTime.of(0, 0).plusSeconds(du.getSeconds());
-		
-		System.out.println("HOUR=" + tmpTime.getHour());
-		System.out.println("MINUTE=" + tmpTime.getMinute());
-		System.out.println("SECOND=" + tmpTime.getSecond());
-		System.out.println("NANO=" + tmpTime.getNano());
+		for(String p : patternArr) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern(p);
+			System.out.println(zdateTime.format(formatter));
+		}
 	}
 }
