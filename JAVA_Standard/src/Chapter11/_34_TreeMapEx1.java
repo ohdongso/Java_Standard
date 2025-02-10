4944f34a43eacbf852795026839b9d756165d8f1
dongso <ehdth5806@naver.com>
2025-02-10 22:53:54 +0900

    자바의정석 213번째 Commit

--- JAVA_Standard/src/Chapter11/_34_TreeMapEx1.java
+++ JAVA_Standard/src/Chapter11/_34_TreeMapEx1.java
@@ -1,6 +1,7 @@
 package Chapter11;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class _34_TreeMapEx1 {
@@ -23,9 +24,28 @@ public class _34_TreeMapEx1 {
 		// 여기서 부터
 		Iterator it = map.entrySet().iterator();
 		
+		System.out.println("= 기본정렬 =");
+		while(it.hasNext()) {
+			Map.Entry entry = (Map.Entry)it.next();
+			int value = ((Integer)entry.getValue()).intValue();
+			System.out.println(entry.getKey() + " : " + printBar('#', value) + " " + value);
+		} // while문 끝.
+		System.out.println();
+		
+		// map을 ArrayList로 변환한 다음에 Collections.sort()로 정렬
 		
 		
 		
 	} // main의 끝.
+	
+	public static String printBar(char ch, int value) {
+		char[] bar = new char[value];
+		
+		for(int i = 0; i < bar.length; i++) {
+			bar[i] = ch;
+		}
+		
+		return new String(bar);
+	}
 	
 } // class
