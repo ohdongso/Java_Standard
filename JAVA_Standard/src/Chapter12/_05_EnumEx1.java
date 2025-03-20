3443abfb6e5495145e1f482d672f912153970a3e
dongso <ehdth5806@naver.com>
2025-03-20 22:51:02 +0900

    자바의정석 231번째 Commit

---
+++ JAVA_Standard/src/Chapter12/_05_EnumEx1.java
@@ -0,0 +1,22 @@
+package Chapter12;
+
+enum Direction { EAST, SOUTH, WEST, NORTH }
+
+public class _05_EnumEx1 {
+	
+	public static void main(String[] args) {
+		
+		Direction d1 = Direction.EAST;
+		Direction d2 = Direction.valueOf("WEST");
+		Direction d3 = Direction.valueOf(Direction.class, "EAST");
+		
+		System.out.println("d1=" + d1);
+		System.out.println("d2=" + d2);
+		System.out.println("d3=" + d3);
+		
+		System.out.println("d1==d2 ? " + (d1 == d2));
+		System.out.println("d1==d3 ? " + (d1 == d3));
+		System.out.println("d1.equals(d3) ? " + d1.equals(d3));
+		
+	} // main
+}
