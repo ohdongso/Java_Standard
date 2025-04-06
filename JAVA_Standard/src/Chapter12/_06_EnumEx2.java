54954e0b908c832e2acbac2c164bae3537ff7bf5
dongso <ehdth5806@naver.com>
2025-04-06 22:36:38 +0900

    자바의정석 236번째 Commit

--- JAVA_Standard/src/Chapter12/_06_EnumEx2.java
+++ JAVA_Standard/src/Chapter12/_06_EnumEx2.java
@@ -42,8 +42,19 @@ public class _06_EnumEx2 {
 	
 	public static void main(String[] args) {
 		
-		// ==> 여기서 부터
+		for(Direction1 d : Direction1.values()) {
+			System.out.printf("%s=%d%n", d.name(), d.getValue());
+		}
 		
+		Direction1 d1 = Direction1.EAST;
+		Direction1 d2 = Direction1.of(1);
 		
+		System.out.printf("d1=%s, %d%n", d1.name(), d1.getValue());
+		System.out.printf("d2=%s, %d%n", d2.name(), d2.getValue());
+		
+		System.out.println(Direction1.EAST.rotate(1));
+		System.out.println(Direction1.EAST.rotate(2));
+		System.out.println(Direction1.EAST.rotate(-1));
+		System.out.println(Direction1.EAST.rotate(-2));
 	} // main
 }
